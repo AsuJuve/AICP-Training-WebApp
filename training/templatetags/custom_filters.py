4cf341bc8b1351a06bd5131143ecfae4c9bc5955
AsuJuve <superjuve@outlook.es>
2023-12-19 03:53:42 -0600

    Improved recommendation table and categories list

--- training/templatetags/custom_filters.py
+++ training/templatetags/custom_filters.py
@@ -1,8 +1,15 @@
 # custom_filters.py
 from django import template
+from math import ceil
 
 register = template.Library()
 
 @register.filter(name='dict_lookup')
 def dict_lookup(dictionary, key):
     return dictionary.get(key, None)
+
+@register.filter(name='denormalize_difficulty')
+def denormalize_difficulty(normalized):
+    denormalized = ceil(normalized * (3500 - 800) + 800)
+
+    return denormalized
